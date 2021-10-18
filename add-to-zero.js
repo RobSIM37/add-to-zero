03f6ac32f6e2406374e1d91ec4f9b18d843d95f3
Robert Lewis <robsim37@gmail.com>
2021-10-18 09:23:38 -0400

    bug fix

--- add-to-zero.js
+++ add-to-zero.js
@@ -1,5 +1,6 @@
 
-
+let array = [28, 43, -12, 30, 4, 0, 12]
+console.log(addToZero(array));
 console.log(addToZero([1, 4, 11, 2, 37, -4])); //should log True because 4 and -4 add to 0.
 console.log(addToZero([0, 21, 33, 6, 0, -9])); //should log True because 0 and 0 add to 0.
 console.log(addToZero([0, 1, 2, 3, 4, 5])); //should log False because no two numbers add to 0.
@@ -18,9 +19,9 @@ function addToZero(arr){
             found = true;
         }
 
-        arr = original;
+        arr = [...original];
         i++;
-        
+
     }
 
     return found;
